c3eb4ae97c17366dab5d53a30c2b0d904b15087b
Albin1118 <albinso@student.chalmers.se>
2018-05-03 19:14:05 +0200

    updated

--- src/iMat/Main.java
+++ src/iMat/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("iMat");
         primaryStage.setScene(new Scene(root, 1280, 880));
         primaryStage.show();
     }
