f0819328b56cd09a5d1ffd34cc18462844c65707
Ziggy <frmax@student.chalmers.se>
2018-05-04 12:05:53 +0200

    add imat icon

--- src/iMat/Main.java
+++ src/iMat/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"));
         primaryStage.setTitle("iMat");
         primaryStage.setScene(new Scene(root, 1280, 880));
+        primaryStage.getIcons().add(new Image("https://raw.githubusercontent.com/ziggyt/DAT216/master/imat_icon.gif"));
         primaryStage.show();
     }
 
