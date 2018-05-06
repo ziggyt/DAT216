f6d443e136d4e6f8be61c356d30528e7a1e81018
Anders Magnusson <anders14magnusson@gmail.com>
2018-05-06 06:59:07 +0200

    Add base code for ProductListItem

--- src/iMat/ProductListItem.java
+++ src/iMat/ProductListItem.java
@@ -1,4 +1,70 @@
 package iMat;
 
-public class ProductListItem {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.Product;
+
+import java.io.IOException;
+
+public class ProductListItem extends AnchorPane {
+    private Controller parentController;
+    private Product product;
+
+    @FXML private ImageView productImageView;
+    @FXML private Label nameLabel;
+    @FXML private Label priceLabel;
+    @FXML private Button buyButton;
+
+    public ProductListItem(Product product, Controller controller) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("recipe_listitem.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        this.product = product;
+        this.parentController = controller;
+
+        //productImageView.setImage(getSquareImage( /*add product image here*/ ));
+        nameLabel.setText(product.getName());
+        priceLabel.setText(product.getPrice() + " kr");
+    }
+
+    public Image getSquareImage(Image image){
+
+        int x = 0;
+        int y = 0;
+        int width = 0;
+        int height = 0;
+
+        if(image.getWidth() > image.getHeight()){
+            width = (int) image.getHeight();
+            height = (int) image.getHeight();
+            x = (int)(image.getWidth() - width)/2;
+            y = 0;
+        }
+
+        else if(image.getHeight() > image.getWidth()){
+            width = (int) image.getWidth();
+            height = (int) image.getWidth();
+            x = 0;
+            y = (int) (image.getHeight() - height)/2;
+        }
+
+        else{
+            //Width equals Height, return original image
+            return image;
+        }
+        return new WritableImage(image.getPixelReader(), x, y, width, height);
+    }
 }
