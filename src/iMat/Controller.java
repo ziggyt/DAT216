25a12be0c8ab576b4bd55b9cc7168cce4579eb94
Anders Magnusson <anders14magnusson@gmail.com>
2018-05-06 07:09:29 +0200

    Add productListItemMap with all the products

--- src/iMat/Controller.java
+++ src/iMat/Controller.java
@@ -8,11 +8,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    private IMatDataHandler bc = IMatDataHandler.getInstance();
 
     @FXML private ToggleButton allCategoryButton;
     @FXML private ToggleButton favCategoryButton;
@@ -22,8 +28,15 @@ public class Controller implements Initializable {
     @FXML private Label categoryTitleLabel;
     @FXML private Label categoryAmountLabel;
 
+    private Map<String, ProductListItem> productListItemMap = new HashMap<>();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        for (Product product : bc.getProducts()) {
+            ProductListItem productListItem = new ProductListItem(product, this);
+            productListItemMap.put(product.getName(), productListItem);
+        }
 
         final ToggleGroup categoryToggleGroup = new ToggleGroup();
         allCategoryButton.setToggleGroup(categoryToggleGroup);
