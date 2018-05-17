085f252a201d6e58d97b1fe6c6ce359bfac0781d
Ziggy <frmax@student.chalmers.se>
2018-05-17 16:54:23 +0200

    Search listener selects (ALL) category on change and searches all items

--- src/iMat/Controller.java
+++ src/iMat/Controller.java
@@ -302,6 +302,7 @@ public class Controller implements Initializable {
         searchField.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                allCategoryButton.setSelected(true);
                 updateCurrentCategory();
                 shownProducts = searchProducts(newValue);
 
